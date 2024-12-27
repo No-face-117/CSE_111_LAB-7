09ae8726d39c8c39f5b89e40e750a1d56dfcb15a
No-face-117 <sayedmahamudalif@gmail.com>
2024-12-27 23:03:40 +0600

    T-4 Vehicle2010 class

---
+++ Vehicle2010.java
@@ -0,0 +1,28 @@
+// CSE-111 LAB - 7 TASK - 4     []
+// T-4 Vehicle2010 class
+
+public class Vehicle2010 extends Vehicle{
+    public void Vehicle2010(){
+        moveDown();
+        moveLeft();
+    }
+
+    public void moveUpperRight(){
+        moveUp();
+        moveRight();
+    }
+
+    public void moveLowerRight(){
+        moveDown();
+        moveRight();
+    }
+
+    public void moveLowerLeft(){
+        moveDown();
+        moveLeft();
+    }
+
+}
+
+
+
